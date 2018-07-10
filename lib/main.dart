7f6f8a75bf1b6d8d97d76cd6092edb7f5f987fa3
Filip Šmíd <filip.smid@eman.cz>
2018-07-10 15:21:19 +0200

    Enable en and cs location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_events/events_localizations.dart';
 import 'package:flutter_events/widget_factory.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class EventsAppHome extends StatelessWidget {
   @override
@@ -17,7 +19,13 @@ class EventsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WidgetFactory().createAppWidget(
-      title: 'Event\'s app',
+      onGenerateTitle: (context) => EventsLocalizations.of(context).appTitle,
+      localizationsDelegates: [
+        EventsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: EventsLocalizations.supportedLocales,
       home: EventsAppHome(),
     );
   }
