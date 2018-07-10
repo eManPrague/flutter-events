bc053781f256d78ca2389af8b5007cd819fcaaab
Filip Šmíd <filip.smid@eman.cz>
2018-07-10 14:46:34 +0200

    Create demo home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
-void main() => runApp(/*TODO*/);
+import 'package:flutter/widgets.dart';
+import 'package:flutter_events/widget_factory.dart';
+
+class EventsAppHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => WidgetFactory().createScaffold(
+        appBar: WidgetFactory().createAppBar(
+          title: Text('Events'),
+        ),
+        body: Center(
+          child: Text('Centered text'),
+        ),
+      );
+}
+
+class EventsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WidgetFactory().createAppWidget(
+      title: 'Event\'s app',
+      home: EventsAppHome(),
+    );
+  }
+}
+
+void main() => runApp(EventsApp());
--- lib/widget_factory.dart
+++ lib/widget_factory.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 abstract class WidgetFactory {
   static WidgetFactory _instance;
 
@@ -15,8 +19,44 @@ abstract class WidgetFactory {
     }
     return _instance;
   }
+
+  Widget createAppWidget({String title, Widget home});
+
+  Widget createScaffold({PreferredSizeWidget appBar, Widget body});
+
+  PreferredSizeWidget createAppBar({Widget title});
 }
 
-class _AndroidWidgetFactory implements WidgetFactory {}
+class _AndroidWidgetFactory implements WidgetFactory {
+  @override
+  Widget createAppWidget({String title, Widget home}) {
+    return MaterialApp(title: title, home: home);
+  }
 
-class _IOSWidgetFactory implements WidgetFactory {}
+  @override
+  Widget createScaffold({PreferredSizeWidget appBar, Widget body}) {
+    return Scaffold(appBar: appBar, body: body);
+  }
+
+  @override
+  PreferredSizeWidget createAppBar({Widget title}) {
+    return AppBar(title: title);
+  }
+}
+
+class _IOSWidgetFactory implements WidgetFactory {
+  @override
+  Widget createAppWidget({String title, Widget home}) {
+    return MaterialApp(title: title, home: home);
+  }
+
+  @override
+  Widget createScaffold({PreferredSizeWidget appBar, Widget body}) {
+    return CupertinoPageScaffold(navigationBar: appBar, child: body);
+  }
+
+  @override
+  PreferredSizeWidget createAppBar({Widget title}) {
+    return CupertinoNavigationBar(middle: title);
+  }
+}
